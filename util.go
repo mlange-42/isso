f31e8ac3c511f426f2d256fe5cf7d7e770c8bd9b
Martin Lange <44003176+mlange-42@users.noreply.github.com>
2024-04-17 23:08:05 +0200

    Print solutions as a list of samples per time (#6)

--- util.go
+++ util.go
@@ -2,6 +2,7 @@ package isso
 
 import (
 	"fmt"
+	"sort"
 	"strings"
 )
 
@@ -27,5 +28,68 @@ func SolutionTable[F any](solution Solution[F]) string {
 			b.WriteString("\n")
 		}
 	}
+	return b.String()
+}
+
+type timeEntry struct {
+	Samples  int
+	Subjects map[string]int
+}
+
+func SolutionList[F any](solution Solution[F]) string {
+	b := strings.Builder{}
+
+	times := []map[string]timeEntry{}
+
+	matrices := map[string]string{}
+	for _, a := range solution.Actions {
+		for len(times) <= a.Time {
+			times = append(times, map[string]timeEntry{})
+		}
+
+		if a.Reuse == "" {
+			t := times[a.Time]
+			t[a.Matrix] = timeEntry{
+				Samples:  a.Samples,
+				Subjects: map[string]int{a.Subject: a.Samples},
+			}
+			matrices[a.Subject] = a.Matrix
+		}
+	}
+
+	for _, a := range solution.Actions {
+		if a.Reuse != "" {
+			t := times[a.Time]
+			matrix := matrices[a.Reuse]
+			t[matrix].Subjects[a.Subject] = a.Samples
+		}
+	}
+
+	lines := []string{}
+	for i, t := range times {
+		if len(t) == 0 {
+			continue
+		}
+		first := true
+		for matrix, entry := range t {
+			if first {
+				lines = append(lines, fmt.Sprintf("Time = %2d: %4d x %-16s", i, entry.Samples, matrix))
+			} else {
+				lines = append(lines, fmt.Sprintf("           %4d x %-16s", entry.Samples, matrix))
+			}
+			keys := make([]string, 0, len(entry.Subjects))
+			for k := range entry.Subjects {
+				keys = append(keys, k)
+			}
+			sort.Strings(keys)
+			for _, sub := range keys {
+				lines = append(lines, fmt.Sprintf("               %4d x %-16s", entry.Subjects[sub], sub))
+			}
+
+			first = false
+		}
+	}
+	b.WriteString(strings.Join(lines, "\n"))
+
 	return b.String()
 }
