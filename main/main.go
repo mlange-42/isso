4d63b8ce995eee994936ef7dcb52d42e4dee65f5
mlange-42 <martin_lange_@gmx.net>
2024-04-18 01:17:42 +0200

    build simple CLI with JSON input and output

--- main/main.go
+++ main/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"os"
+
+	"github.com/mlange-42/isso"
+	"github.com/mlange-42/isso/fitness"
 )
 
 func main() {
@@ -12,5 +17,33 @@ func main() {
 	}
 
 	file := os.Args[1]
-	_ = file
+
+	jsData, err := os.ReadFile(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	problem := isso.ProblemDef{}
+	err = json.Unmarshal(jsData, &problem)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	p := isso.NewProblem(problem)
+
+	s := isso.NewSolver(
+		&fitness.TripsAndSamplesEvaluator{},
+		&fitness.TripsThenSamples{},
+	)
+
+	if solution, ok := s.Solve(&p); ok {
+		fmt.Fprintf(os.Stderr, "Found %d solution(s)\n\n", len(solution))
+
+		jsData, err := json.MarshalIndent(&solution, "", "    ")
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(string(jsData))
+		return
+	}
+	fmt.Println("No solution found")
 }
