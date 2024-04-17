324593ce0ac50f48d7f53cee0b4f2cdcedea9748
mlange-42 <martin_lange_@gmx.net>
2024-04-18 01:46:11 +0200

    allow for different output formats and pareto

--- main/main.go
+++ main/main.go
@@ -21,6 +21,7 @@ func main() {
 func RootCommand() *cobra.Command {
 	var format string
 	var file string
+	var pareto bool
 
 	root := &cobra.Command{
 		Use:           "isso",
@@ -32,6 +33,11 @@ func RootCommand() *cobra.Command {
 			return nil
 		},
 		RunE: func(cmd *cobra.Command, args []string) error {
+			if file == "" {
+				_ = cmd.Help()
+				return nil
+			}
+
 			jsData, err := os.ReadFile(file)
 			if err != nil {
 				return err
@@ -44,29 +50,61 @@ func RootCommand() *cobra.Command {
 
 			p := isso.NewProblem(problem)
 
+			var comp isso.Comparator[fitness.TripsAndSamplesFitness]
+			if pareto {
+				comp = &fitness.TripsSamplesPareto{}
+			} else {
+				comp = &fitness.TripsThenSamples{}
+			}
+
 			s := isso.NewSolver(
 				&fitness.TripsAndSamplesEvaluator{},
-				&fitness.TripsThenSamples{},
+				comp,
 			)
+			solution, ok := s.Solve(&p)
+			if !ok {
+				fmt.Println("No solution found")
+				return nil
+			}
 
-			if solution, ok := s.Solve(&p); ok {
-				fmt.Fprintf(os.Stderr, "Found %d solution(s)\n\n", len(solution))
+			fmt.Fprintf(os.Stderr, "Found %d solution(s)\n\n", len(solution))
 
-				jsData, err := json.MarshalIndent(&solution, "", "    ")
+			switch format {
+			case "json":
+				jsData, err = json.MarshalIndent(&solution, "", "    ")
 				if err != nil {
 					return err
 				}
 				fmt.Println(string(jsData))
-				return nil
+
+			case "table":
+				for _, sol := range solution {
+					fmt.Println(isso.SolutionTable(sol))
+					fmt.Printf("(%d trips, %d samples)\n", sol.Fitness.Trips, sol.Fitness.Samples)
+					fmt.Println("------------------------------------------------------------")
+				}
+			case "list":
+				for _, sol := range solution {
+					fmt.Println(isso.SolutionList(sol))
+					fmt.Printf("(%d trips, %d samples)\n", sol.Fitness.Trips, sol.Fitness.Samples)
+					fmt.Println("------------------------------------------------------------")
+				}
+			case "fitness":
+				for _, sol := range solution {
+					fmt.Printf("(%d trips, %d samples)\n", sol.Fitness.Trips, sol.Fitness.Samples)
+				}
+
+			default:
+				return fmt.Errorf("unknown format '%s'", format)
 			}
-			fmt.Println("No solution found")
 
 			return nil
 		},
 	}
 
 	root.Flags().StringVarP(&file, "input", "i", "", "Input JSON file")
-	root.Flags().StringVarP(&format, "format", "f", "table", "Output format. One of [table json]")
+	root.Flags().StringVarP(&format, "format", "f", "table", "Output format. One of [json table list fitness]")
+	root.Flags().BoolVarP(&pareto, "pareto", "p", false, "Use pareto optimization criterion")
 
 	return root
 }
